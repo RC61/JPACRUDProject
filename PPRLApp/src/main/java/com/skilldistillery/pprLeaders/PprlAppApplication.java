c50e6d59365542a63e3aad134e68494b162d54a2
RC61 <jetwestpack@gmail.com>
2020-05-18 09:16:36 -0600

    SQL Updates

--- PPRLApp/src/main/java/com/skilldistillery/pprLeaders/PprlAppApplication.java
+++ PPRLApp/src/main/java/com/skilldistillery/pprLeaders/PprlAppApplication.java
@@ -2,9 +2,18 @@ package com.skilldistillery.pprLeaders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PprlAppApplication {
+public class PprlAppApplication extends SpringBootServletInitializer {
+	
+	
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PprlAppApplication.class);
+	  }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(PprlAppApplication.class, args);
